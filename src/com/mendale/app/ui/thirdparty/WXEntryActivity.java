066dbc3c76839e993c150bb6a5fa6d8c40704242
GitHubZx12 <1294296707@qq.com>
2016-04-20 09:48:24 +0800

    分享

---
+++ src/com/mendale/app/ui/thirdparty/WXEntryActivity.java
@@ -0,0 +1,13 @@
+
+package com.mendale.app.ui.thirdparty;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * 微信
+ * @author zx
+ *
+ */
+public class WXEntryActivity extends WXCallbackActivity {
+
+}
