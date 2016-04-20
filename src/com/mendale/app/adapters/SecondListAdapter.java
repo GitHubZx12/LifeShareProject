6dce0d4c00de25f38f57e76ccaa23dd7ae141d40
GitHubZx12 <1294296707@qq.com>
2016-04-20 09:47:15 +0800

    搜索

---
+++ src/com/mendale/app/adapters/SecondListAdapter.java
@@ -0,0 +1,69 @@
+package com.mendale.app.adapters;
+
+import java.util.List;
+
+import com.mendale.app.R;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SecondListAdapter extends BaseAdapter {
+
+	@SuppressWarnings("unused")
+	private Context context;
+	/** 数据 */
+	private List<String> list;
+	private LayoutInflater inflater;
+
+	public SecondListAdapter(Context context, List<String> list) {
+		this.context = context;
+		this.list = list;
+		inflater = LayoutInflater.from(context);
+	}
+
+	@Override
+	public int getCount() {
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@SuppressLint("InflateParams")
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder = null;
+		if (convertView == null) {
+			holder = new ViewHolder();
+			convertView = inflater.inflate(R.layout.item_second_list, null);
+			holder.imageViewSecondListIcon = (ImageView) convertView.findViewById(R.id.imageViewSecondListIcon);
+			holder.textViewSecondListName = (TextView) convertView.findViewById(R.id.textViewSecondListName);
+			convertView.setTag(holder);
+		}
+		else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.textViewSecondListName.setText(list.get(position));
+//		holder.imageViewSecondListIcon.setBackgroundResource(list.get(position).getBg());
+		return convertView;
+	}
+
+	class ViewHolder {
+
+		ImageView imageViewSecondListIcon;
+		TextView textViewSecondListName;
+	}
+}
